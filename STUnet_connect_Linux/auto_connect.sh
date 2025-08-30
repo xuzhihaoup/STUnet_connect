03e0f59c7cc703e83d4d02b2f0ae9411c9d970db
xuzhihaouuu <91545024+xuzhihaoup@users.noreply.github.com>
2025-08-30 12:50:30 +0800

    Add files via upload

---
+++ STUnet_connect_Linux/auto_connect.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+# 执行 Python 脚本
+#/home/zhxu/STUnet_connect-main/auto_connect.py   请将此路径修改为你自己的路径
+python3 /home/zhxu/STUnet_connect-main/auto_connect.py #> /dev/null 2>&1 &
+
---
+++ STUnet_connect_Linux/hand_connect.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+# 执行 Python 脚本
+python3 main.py
+
